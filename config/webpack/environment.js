3f337f1eba7222fa3fe63fbf18623538de51f957
kodama risa <retedg623@gmail.com>
2024-05-18 12:20:54 +0900

    エラーへの対応

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,8 @@
 const { environment } = require('@rails/webpacker')
 
+const webpack = require('webpack')
+environment.plugins.append('Provide', new webpack.ProvidePlugin({
+  Stimulus: ['@stimulus/core', 'default']
+}))
+
 module.exports = environment
