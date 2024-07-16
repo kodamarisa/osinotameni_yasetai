bcf6d9c3267ef823a595f4eb6ca21d2dd2688189
kodama risa <retedg623@gmail.com>
2024-07-16 16:56:12 +0900

    カレンダーモデルの修正

--- app/models/calendar.rb
+++ app/models/calendar.rb
@@ -1,4 +1,6 @@
 class Calendar < ApplicationRecord
+  belongs_to :user, polymorphic: true
+
   has_many :calendar_users, dependent: :destroy
   has_many :users, through: :calendar_users, source: :user, source_type: 'User'
   has_many :line_users, through: :calendar_users, source: :user, source_type: 'LineUser'
